ef77b893b83ac9b9f952a117141a5e1b70a75c67
aeiboeck <aeiboeck@eurofunk.com>
2023-12-14 15:45:33 +0100

    minor things

---
+++ adventDay4/day4.h
@@ -0,0 +1,16 @@
+//
+// Created by aeiboeck on 14.12.2023.
+//
+
+#ifndef ADVENT1_DAY4_H
+#define ADVENT1_DAY4_H
+#include <iostream>
+
+class day4 {
+public:
+    void dataFeeder();
+    int calculatePoints(std::string data);
+};
+
+
+#endif //ADVENT1_DAY4_H
